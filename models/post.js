ee061b759820747a79ac98736f7c4ee7031c08ce
ixiaoluo <584188065@qq.com>
2014-05-04 09:50:58 +0800

    add user and article

--- models/post.js
+++ models/post.js
@@ -57,7 +57,7 @@ Post.prototype.save = function(callback) {
 };
 
 //读取文章及其相关信息
-Post.get = function(name, callback) {
+Post.getAll = function(name, callback) {
 
   //打开数据库
   mongodb.open(function(err, db) {
@@ -94,3 +94,36 @@ Post.get = function(name, callback) {
     });
   });
 };
+
+Post.getOne = function(name, day, title, callback) {
+	//打开数据库
+	mongodb.open(function(err, db) {
+		if(err) {
+			return callback(err);
+		}
+
+		// 读取posts集合
+		db.collection('posts', function(err, collection) {
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+
+			//根据用户名,发表日期及文章名进行查询
+			collection.findOne({
+				'name': name,
+				'time.day': day,
+				'title': title
+			}, function(err, doc) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+			
+				// 解析markdown为html
+				doc.post = markdown.toHTML(doc.post);
+				return callback(null, doc); //返回查询的一篇文章
+			});
+		});
+	});
+}
